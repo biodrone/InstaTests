3137453df6bdc088735fdfcaa364f7ed1984519c
Josh Jacobs <josh.jacobs19@gmail.com>
2015-08-22 13:54:03 +0100

    Testing things with Brown

--- nltk_test.py
+++ nltk_test.py
@@ -2,6 +2,7 @@
 
 import nltk
 from nltk.corpus import treebank
+from nltk.corpus import brown
 
 sentence = """At eight o'clock on Thursday morning Arthur didn't feel very good."""
 tokens = nltk.word_tokenize(sentence)
@@ -11,5 +12,11 @@ print tagged[0:6]
 entities = nltk.chunk.ne_chunk(tagged)
 print entities
 
-t = treebank.parsed_sents('wsj_0001.mrg')[0]
-t.draw()
+#t = treebank.parsed_sents('wsj_0001.mrg')[0]
+#t.draw()
+
+
+print brown.words()[0:10]
+print brown.tagged_words()[0:10]
+print len(brown.words())
+print dir(brown)
