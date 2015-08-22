9a104c2860589b08e36b156bc84ea502382cce52
Josh Jacobs <josh.jacobs19@gmail.com>
2015-08-22 18:04:45 +0100

    currently works to find the latest media using a certain tag

--- instaAPI.py
+++ instaAPI.py
@@ -8,20 +8,21 @@ from collections import OrderedDict
 from urllib2 import urlopen
 
 user_id = "2071972233"
-user_token = "2071972233.ab103e5.04668cab8f264287b09e74030d24050c "
+user_token = "2071972233.ab103e5.04668cab8f264287b09e74030d24050c"
+client_id = '311da96cc1b7487a83a0fadb2dc6b464'
+client_secret = '4394245572c74f11aa0b2046974e9f65'
+access_token = '2071972233.311da96.836a5a01111d4c27949e639f30f94673'
+client_ip = urlopen('http://ip.42.pl/raw').read()
 
 def main():
-    client_id = '311da96cc1b7487a83a0fadb2dc6b464'
-    client_secret = '4394245572c74f11aa0b2046974e9f65'
-    access_token = '2071972233.311da96.836a5a01111d4c27949e639f30f94673'
-    client_ip = urlopen('http://ip.42.pl/raw').read()
-    api = InstagramAPI(client_id=client_id, client_secret=client_secret,client_ips= client_ip,access_token= access_token)
+
+    print client_ip
+    api = InstagramAPI(client_id=client_id, client_secret=client_secret, client_ips=client_ip, access_token=access_token)
 
     all_media_ids = []
-    media_ids,next = api.tag_recent_media(tag_name="instagood", count=80)
+    media_ids,next = api.tag_recent_media(tag_name='like4like', count=1000)
     temp,max_tag=next.split('max_tag_id=')
     max_tag=str(max_tag)
-    return
 
 
     for media_id in media_ids:
@@ -31,7 +32,7 @@ def main():
     counter = 1
 
     while next and counter < 3:
-    	more_media, next =api.tag_recent_media(tag_name='instadogs', max_tag_id=max_tag)
+    	more_media, next =api.tag_recent_media(tag_name='like4like', max_tag_id=max_tag)
     	temp,max_tag=next.split('max_tag_id=')
     	max_tag=str(max_tag)
     	for media_id2 in more_media:
@@ -39,9 +40,9 @@ def main():
     	counter+=1
 
     #remove duplictes if any.
-    media_all_ids=list(OrderedDict.fromkeys(media_all_ids))
+    all_media_ids=list(OrderedDict.fromkeys(all_media_ids))
 
-    print len(media_all_ids)
+    print len(all_media_ids)
 
 
 if __name__ == "__main__":
